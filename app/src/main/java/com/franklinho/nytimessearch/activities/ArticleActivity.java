72c7278d052d5636a8506ea96d57c07c3736db34
Franklin Ho <franklinho@Franklins-MacBook-Pro.local>
2016-02-09 18:26:09 -0800

    added sharing button to webview

--- app/src/main/java/com/franklinho/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/com/franklinho/nytimessearch/activities/ArticleActivity.java
@@ -1,10 +1,16 @@
 package com.franklinho.nytimessearch.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ShareActionProvider;
 
 import com.franklinho.nytimessearch.Article;
 import com.franklinho.nytimessearch.R;
@@ -16,7 +22,8 @@ import butterknife.ButterKnife;
 
 public class ArticleActivity extends AppCompatActivity {
     @Bind(R.id.wvArticle) WebView wvArticle;
-
+    Article article;
+    private ShareActionProvider miShareAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,8 @@ public class ArticleActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Article article = (Article) Parcels.unwrap(getIntent().getParcelableExtra("article"));
+        article = (Article) Parcels.unwrap(getIntent().getParcelableExtra("article"));
+
 
         wvArticle.setWebViewClient(new WebViewClient(){
             @Override
@@ -38,4 +46,18 @@ public class ArticleActivity extends AppCompatActivity {
         wvArticle.loadUrl(article.getWebUrl());
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_article, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        android.support.v7.widget.ShareActionProvider miShare = (android.support.v7.widget.ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+        miShare.setShareIntent(shareIntent);
+
+        return super.onCreateOptionsMenu(menu);
+    }
 }
